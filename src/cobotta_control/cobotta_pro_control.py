e2d675de2652847d9217ce068a6b88cfda7df8a1
t_kubo <t_kubo.tome@outlook.jp>
2025-07-04 17:55:44 +0900

    Wait for vr to follow robot after tool change

--- src/cobotta_control/cobotta_pro_control.py
+++ src/cobotta_control/cobotta_pro_control.py
@@ -669,6 +669,9 @@ class Cobotta_Pro_CON:
                         self.pose[18] = 1
                         self.pose[17] = 0
                         self.logger.info("Tool change succeeded")
+                        # NOTE: より良い方法がないか
+                        # ロボットとVRを同期させる
+                        time.sleep(5)
                     except Exception as e:
                         self.logger.error("Error during tool change")
                         self.logger.error(f"{self.robot.format_error(e)}")
@@ -681,6 +684,9 @@ class Cobotta_Pro_CON:
                     # 成功しても失敗してもループを継続する (ツールを変えることによる
                     # 予測できないエラーは起こらないため)
                     self.demo_put_down_box()
+                    # NOTE: より良い方法がないか
+                    # ロボットとVRを同期させる
+                    time.sleep(5)                    
                 # 単なる停止が要求された場合は、ループを抜ける
                 else:
                     break
